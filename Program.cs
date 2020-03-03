3d7ead8d2559b38f57a27682b8154dea8e9d7e1c
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2020-03-03 17:03:04 -0600

    updated code

--- Program.cs
+++ Program.cs
@@ -13,18 +13,6 @@ namespace MonsterCSV
             //pulling the files from the filePath and reading the CSV files
             string filePath = @"/Users/Heidi/Downloads/two copy.csv";
 
-            // List<string> lines = File.ReadAllLines(filePath).ToList();
-            // foreach (var line in lines)
-            // {
-            //     Console.WriteLine(line);
-            // }
-            // Adding files to the CSV file
-
-            // lines.Add("2020-3-3 14:30:15, Heidi Smith, Cthulu, 46.057457, -120.088088");
-            // File.WriteAllLines(filePath, lines);
-            // Console.ReadLine();
-
-
             //Step 1: how to access the data from the text file to populate a list of monsters
 
             List<Monster> monsters = new List<Monster>();
